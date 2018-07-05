2875f9fb93169259b2e0d2095f3f53282b30acc9
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2018-07-05 18:48:45 +0200

    added referee and player views
    added referee and player views with buttons and modules

---
+++ js/BubbleChartViewPlayers.js
@@ -0,0 +1,13 @@
+/* eslint-env browser */
+
+var BubbleChart = BubbleChart | {};
+
+BubbleChart.BubbleChartViewPlayers = function () {
+  "use strict";
+  
+  var that = {};
+
+  /* Darstellung ... */
+
+  return that;
+};
---
+++ js/BubbleChartViewReferees.js
@@ -0,0 +1,13 @@
+/* eslint-env browser */
+
+var BubbleChart = BubbleChart | {};
+
+BubbleChart.BubbleChartViewReferees = function () {
+  "use strict";
+  
+  var that = {};
+
+  /* Darstellung ... */
+
+  return that;
+};
